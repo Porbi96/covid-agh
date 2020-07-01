ab5dfc4f78db627ced5ba429595c6f0a205a2485
Porbi96 <spikasny@gmail.com>
2020-07-01 18:08:49 +0200

    Added protection options to SEIR model

--- SEIR/seir_model.m
+++ SEIR/seir_model.m
@@ -4,21 +4,60 @@ I1_0 = 10; % initial number of infected
 T = 300; % period of 300 days
 dt = 1/4; % time interval of 6 hours (1/4 of a day)
 
-sigma = 0.9;
-delta = 0.2;
-eta1 = 0.05;
-eta2 = 0.4;
-gamma1 = 0.2;
-gamma2 = 0.05;
-lambda1 = 0.6;
-lambda2 = 0.8;
-kappa1 = 0.02;
-kappa2 = 0.3;
+isSelfProtection = 0;
+isPublicProtection = 0;
+ventilators = 25000;
 
-beta = 1; % Must be 1;
+if (isSelfProtection && isPublicProtection)
+    sigma = 0.35;
+    delta = 0.02;
+    eta1 = 0.2;
+    eta2 = 0.8;
+    gamma1 = 0.2;
+    gamma2 = 0.05;
+    lambda1 = 0.6;
+    lambda2 = 0.8;
+    kappa1 = 0.02;
+    kappa2 = 0.2;
+elseif (~isSelfProtection && isPublicProtection)
+    sigma = 0.7;
+    delta = 0.02;
+    eta1 = 0.2;
+    eta2 = 0.8;
+    gamma1 = 0.2;
+    gamma2 = 0.05;
+    lambda1 = 0.6;
+    lambda2 = 0.8;
+    kappa1 = 0.02;
+    kappa2 = 0.2;
+elseif (isSelfProtection && ~isPublicProtection)
+    sigma = 0.4;
+    delta = 0.02;
+    eta1 = 0.1;
+    eta2 = 0.3;
+    gamma1 = 0.2;
+    gamma2 = 0.05;
+    lambda1 = 0.6;
+    lambda2 = 0.8;
+    kappa1 = 0.02;
+    kappa2 = 0.2;
+else
+    sigma = 0.9;
+    delta = 0.02;
+    eta1 = 0.01;
+    eta2 = 0.2;
+    gamma1 = 0.2;
+    gamma2 = 0.05;
+    lambda1 = 0.6;
+    lambda2 = 0.8;
+    kappa1 = 0.02;
+    kappa2 = 0.2;
+end
+
+beta = 0.1;
 
 % Calculate the model
-[S,E,I1,I2,Q,H,R,D] = sir_model(sigma,beta,delta,eta1,eta2,gamma1,gamma2,lambda1,lambda2,kappa1,kappa2,Npop,I1_0,T,dt);
+[S,E,I1,I2,Q,H,R,D] = sir_model(sigma,beta,delta,eta1,eta2,gamma1,gamma2,lambda1,lambda2,kappa1,kappa2,Npop,I1_0,ventilators,T,dt);
 
 % Plots that display the epidemic outbreak
 tt = 0:dt:T-dt;
@@ -27,7 +66,7 @@ plot(tt,S,tt,I1+I2,tt,Q,tt,H,tt,R,tt,D,'LineWidth',1); grid on;
 xlabel('Days'); ylabel('Number of individuals');
 legend('S','I','Q', 'H', 'R', 'D');
 
-function [S,E,I1,I2,Q,H,R,D] = sir_model(sigma,beta,delta,eta1,eta2,gamma1,gamma2,lambda1,lambda2,kappa1,kappa2,Npop,I1_0,T,dt)
+function [S,E,I1,I2,Q,H,R,D] = sir_model(sigma,beta,delta,eta1,eta2,gamma1,gamma2,lambda1,lambda2,kappa1,kappa2,Npop,I1_0,vents,T,dt)
     S = zeros(1,T/dt);
     E = zeros(1,T/dt);
     I1 = zeros(1,T/dt);
@@ -49,7 +88,7 @@ function [S,E,I1,I2,Q,H,R,D] = sir_model(sigma,beta,delta,eta1,eta2,gamma1,gamma
         dQ = (eta1*I1(tt) + eta2*I2(tt) - gamma2*Q(tt) - lambda1*Q(tt) - kappa1*Q(tt)) * dt;
         dH = (gamma1*I2(tt) + gamma2*Q(tt) - lambda2*H(tt) - kappa2*H(tt)) * dt;
         dR = (lambda1*Q(tt) + lambda2*H(tt)) * dt;
-        dD = (kappa1*Q(tt) + kappa2*H(tt)) * dt;
+        dD = (kappa1*Q(tt) + (2*(vents<H(tt)+1))*kappa2*H(tt)) * dt;
         S(tt+1) = S(tt) + dS;
         E(tt+1) = E(tt) + dE;
         I1(tt+1) = I1(tt) + dI1;
